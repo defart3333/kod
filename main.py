8e1714fa2b1f555f1efe4efbda69daaae07bae8d
defart3333 <88496011+defart3333@users.noreply.github.com>
2021-08-05 19:58:00 +0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env pybricks-micropython
+from turtle import *
+color('green')
+bgcolor('black')
+speed(11)
+hideturtle()
+b = 0 
+while b < 200:
+    right(b)
+    forward(b *3)
+    b += 1
+    
